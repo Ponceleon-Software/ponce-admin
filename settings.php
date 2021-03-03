3af0cf437f8ffcbf6e88824454c2ea495e74d0dc
Cesar Pérez <cesaraugp01@gmail.com>
2021-03-03 19:24:43 -0300

    close #36 close #30 correcciones folder, db y json

--- settings.php
+++ settings.php
@@ -4,18 +4,19 @@ $properties = array(
         array(
             'name' => 'ponceTopBar',
             'is_active'=>false,
-            'value' => 'valor'
-           
-        
+            'options' => 'valor',
+            'keywords'=>array("top bar", "barra superior", "admin bar")
+                   
         ),
         array(
             'name' => 'ponceLogo',
             'is_active'=>false,
-            'value' =>array(		
+            'options' =>array(		
     		"src" =>'false',
     		"inAdmin" =>'false',
     		"inLogin" =>'false',
-    		)
+    		),
+            'keywords'=>array("logo", "imagen", "image")
            
         )
     );
