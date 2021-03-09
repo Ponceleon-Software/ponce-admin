36db69ed10729a097069a183bd5d4b7510318776
angeljsb <suangel.007@gmail.com>
2021-03-09 19:16:09 -0400

    borrar datos de la bd al ingresar vacio

--- endpoints/poncelogo.php
+++ endpoints/poncelogo.php
@@ -10,15 +10,29 @@ function setPonceLogo(WP_REST_request $request){
 
   //Definitions
   $uploadsFolder = str_replace("\\endpoints", "/assets/img/", __DIR__ );
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'ponce';
 
   $file = $request->get_file_params()["image"];
-  $fileTmpPath = $file['tmp_name'];
-  $fileName = $file['name'];
-
-  $dest_path = $uploadsFolder . $fileName;
-  if(move_uploaded_file($fileTmpPath, $dest_path)){
-    return true;
+  if(isset($file) && is_uploaded_file($file['tmp_name'])){
+    $fileTmpPath = $file['tmp_name'];
+    $fileName = $file['name'];
+  
+    $dest_path = $uploadsFolder . $fileName;
+    if(!move_uploaded_file($fileTmpPath, $dest_path)){
+      return false;
+    }
+  }else{
+    $result=$wpdb->update(
+      $table_name, 
+      array('options' => 'src:,inAdmin:false,inLogin:false'), 
+      array( 'name' => 'ponceLogo' )
+    );
+    if($result>0){
+      return true;
+    }
   }
-	return false;
+  
+	return true;
 }
 ?>
