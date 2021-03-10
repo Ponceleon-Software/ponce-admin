444ad346ea46c92d87b21ac70a2cfd07903ca6df
angeljsb <suangel.007@gmail.com>
2021-03-10 12:44:48 -0400

    endpoint para activar solutions close #29

--- endpoints/activate.php
+++ endpoints/activate.php
@@ -1,26 +1,20 @@
 <?php
 
 add_action( 'rest_api_init', function () {
-  global $todb;
-  foreach ($todb as $value) {
-    $name = $value['name'];
-    register_rest_route( 'ponceadmin/v2', $name, array(
-      'methods' => 'GET',
-      'callback' => function(){ hideTopbar($name); },
-    ));
-  }
-  register_rest_route( 'ponceadmin/v2', 'topbar', array(
+  register_rest_route( 'ponceadmin/v2', "/activate/(?P<name>.+)", array(
     'methods' => 'GET',
     'callback' => 'hideTopbar',
-    ));
-} );
-function hideTopbar($name){
-	global $wpdb;
-	$table_name = $wpdb->prefix . 'ponce';
-	$value = $wpdb->get_var( $wpdb->prepare(
+  ));
+}
+);
+function hideTopbar($data){
+  $name = $data->get_param('name');
+  global $wpdb;
+  $table_name = $wpdb->prefix . 'ponce';
+  $value = $wpdb->get_var( $wpdb->prepare(
     " SELECT is_active FROM $table_name WHERE name = '$name' "
-	));
-	$result=$wpdb->update($table_name, array('is_active' => !$value), array( 'name' => $name ));
-	return $result;
+  ));
+  $result=$wpdb->update($table_name, array('is_active' => !$value), array( 'name' => $name ));
+  return $result;
 }
 ?>
